2693694b95ac24534bd04d3622296b710bd694e8
Gene Tigner <genetigner.art@gmail.com>
2019-04-14 01:21:57 -0500

    code for random things
    code for random things

---
+++ Assets/randomObjects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomObjects : MonoBehaviour
+{
+    private GameObject RandomItem = null;
+    [SerializeField] GameObject[] objs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int index = Random.Range(0, objs.Length-1);
+        RandomItem = objs[index];
+
+        Debug.Log(index);
+        this.gameObject.GetComponent<MeshFilter>().mesh = RandomItem.GetComponent<MeshFilter>().mesh;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
